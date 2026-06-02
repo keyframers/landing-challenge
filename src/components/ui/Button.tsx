2c1e0791df64a9c006597b8f5215c50930d92773
Stephen Shaw <shshaw@users.noreply.github.com>
2026-06-02 14:22:01 -0700

    Border anim

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,33 +9,21 @@ import { CustomEase } from "gsap/CustomEase";
 import PhysicsPropsPlugin from "gsap/PhysicsPropsPlugin";
 import Physics2DPlugin from "gsap/Physics2DPlugin";
 
-gsap.registerPlugin(
-  useGSAP,
-  SplitText,
-  CustomEase,
-  PhysicsPropsPlugin,
-  Physics2DPlugin,
-);
+gsap.registerPlugin(useGSAP, SplitText, CustomEase, PhysicsPropsPlugin, Physics2DPlugin);
 
 type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
   large?: boolean;
   subtle?: boolean;
 };
 
-export default function Button({
-  className,
-  large,
-  subtle,
-  children,
-  ...props
-}: ButtonProps) {
+export default function Button({ className, large, subtle, children, ...props }: ButtonProps) {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useGSAP(
     () => {
       if (!containerRef.current) return;
     },
-    { scope: containerRef },
+    { scope: containerRef }
   );
 
   return (
